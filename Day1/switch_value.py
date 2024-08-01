310f492ab31643416b609c502ca5aca7657c6068
Aparna Warade <aparna.warade@outlook.com>
2024-08-01 16:36:15 -0400

    Create switch_value.py

---
+++ Day1/switch_value.py
@@ -0,0 +1,26 @@
+"""This program takes two inputs. The first input is stored in a variable called a. The second input is stored in a variable called b.
+
+Write a program that switches the values stored in the variables a and b.
+
+Warning . You don't need to print anything. The print statement is already in the template code. However, your program should work for different inputs. e.g. any value of a and b.
+
+Example Input 1
+29
+41
+Example Output 1
+a: 41
+b: 29
+"""
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+a,b=b,a
+
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
