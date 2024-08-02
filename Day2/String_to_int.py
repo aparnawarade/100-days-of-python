721cede66a543e727a7235b863935e7d522de86e
Aparna Warade <aparna.warade@outlook.com>
2024-08-02 16:55:22 -0400

    Create String_to_int.py

---
+++ Day2/String_to_int.py
@@ -0,0 +1,9 @@
+two_digit_number = input()
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+# Add the two integers together
+two_digit_number = first_digit + second_digit
+
+print(two_digit_number)
