caa87575cfbe41b2aa4ceef276c93671fb7bb70c
Aparna Warade <aparna.warade@outlook.com>
2024-08-27 17:22:35 -0400

    Add files via upload

---
+++ Day4/random_numbers.py
@@ -0,0 +1,12 @@
+import random
+random_integer=random.randint(1,10)
+print(random_integer)
+
+random_float=random.random() #0.0 to <1.0
+print(random_float)
+
+random_float=random.random()*5 #0.0 to <5.0
+print(random_float)
+
+uniform_random=random.uniform(10,50) #1.0 to <5.0
+print(uniform_random)
