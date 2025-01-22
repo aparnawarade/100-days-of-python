e687eafee8abf8d5c4fcbae5e8d376c2ebecafcf
Aparna Warade <aparna.warade@outlook.com>
2025-01-22 11:20:45 -0500

    Update bid.py

--- Day9/bid.py
+++ Day9/bid.py
@@ -1,1 +1,21 @@
+def findhighestbid(bidding):
+    highest_bid=0 
+    winner=""
+    for name in bidding:
+       if bidding[name]>highest_bid:
+           highest_bid=bidding[name]
+           winner=name
+    print(f"Winner is {winner} with bid {highest_bid}")
+
+bidding={}
+bid=True
+while bid:
+    name = input('Enter Name: ')
+    bid_price = float(input('Bid price: '))
+    bidding[name]=bid_price
+    ans=input("More Users want to bid :")
+    if ans.lower()!="yes":
+        bid=False
+        findhighestbid(bidding)
+
 
